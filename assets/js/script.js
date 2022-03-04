059e2fc80460baa80ea39034ccf471bf038947e8
koiclone <cheryljcruz1@gmail.com>
2022-03-04 16:07:14 -0600

    added psuedocode and eventlistener for start button

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Create variables to store the quiz questions
+// Use mouse click events to start quiz
+// - when clicked startQuiz: start timer, generate quiz
+// Write for loops to cycle through quiz questions
+// Use key press events to receive user input in the form of answers to quiz questions
+// Create a time limit for the game using time functions
+// Write conditional statements to determine right and wrong answers
+// Use client side storage to store highscores
+var startButton = document.querySelector("#start-btn");
+var timeEl = document.querySelector("#timer");
+
+
+
+var startQuiz = function (event) {
+
+}
+
+
+// Click event start
+startButton.addEventListener("click", startQuiz);
